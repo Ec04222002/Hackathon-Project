4e4be350fdac088730e203f7db509c9b1fec7bb1
Alex Nguyen <123827639+alexn435@users.noreply.github.com>
2023-01-28 23:53:30 +0000

    a

--- widgets/ticker_graph.py
+++ widgets/ticker_graph.py
@@ -1,12 +1,18 @@
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 class TickerGraph:
 
-    def __init__(self, ticker_name, start_date, time_span):
+    def __init__(self, ticker_name, start_date, time_x, price_y):
         self.ticker_name = ticker_name
         self.start_date = start_date
-        self.time_span = time_span
+        self.time_x = time_x
+        self.price_y = price_y
+        
 
     def make_graph(self):
-        print(self.ticker_name)
+        
+        
+          
+        
         pass
